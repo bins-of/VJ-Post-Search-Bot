d2a2a3aaf53bf64a0c614a228bbaab62a75b4fb1
bins-of <binsofbins1@gmail.com>
2025-03-03 16:37:45 +0600

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+API_ID       = int(environ.get("API_ID", "26649585"))
+API_HASH     = environ.get("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7933029302:AAEvjvNgn3rdImU43n-fLUZTf8kerIxx_Dk")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://rohanahamed75:gt4RXJZ1mUtOh4Xv@mmtg.0ong5.mongodb.net/?retryWrites=true&w=majority&appName=mmtg")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002316472437"))
+ADMIN        = int(environ.get("ADMIN", "7822720438"))
+CHANNEL      = environ.get("CHANNEL", "@RM_Movie_Flix")
